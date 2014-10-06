b087fbf2d1379715a716a2211af9b3af24e2dde6
myun2 <myun2@nwhite.info>
2014-10-06 19:40:07 +0900

    greater_than spec (Numericality validator)

--- spec/validations/numericality_spec.rb
+++ spec/validations/numericality_spec.rb
@@ -1,23 +1,55 @@
 require 'shibuya_record/validations/numericality'
 
 describe ShibuyaRecord::Validations::Numericality do
-  let(:validator) { ShibuyaRecord::Validations::Numericality.new }
+  context "not present options." do
+    let(:validator) { ShibuyaRecord::Validations::Numericality.new }
 
-  it { expect(validator.valid? nil).to eq false }
-  it { expect(validator.valid? []).to eq false }
-  it { expect(validator.valid?({})).to eq false }
-  it { expect(validator.valid? "").to eq false }
+    it { expect(validator.valid? nil).to eq false }
+    it { expect(validator.valid? []).to eq false }
+    it { expect(validator.valid?({})).to eq false }
+    it { expect(validator.valid? "").to eq false }
 
-  it { expect(validator.valid? 0).to eq true }
-  it { expect(validator.valid? 3).to eq true }
-  it { expect(validator.valid? 4.6).to eq true }
-  it { expect(validator.valid? "3").to eq true }
-  it { expect(validator.valid? "5.6").to eq true }
-  it { expect(validator.valid? -3).to eq true }
-  it { expect(validator.valid? -4.6).to eq true }
-  it { expect(validator.valid? "-3").to eq true }
-  it { expect(validator.valid? "-5.6").to eq true }
+    it { expect(validator.valid? 0).to eq true }
+    it { expect(validator.valid? 3).to eq true }
+    it { expect(validator.valid? 4.6).to eq true }
+    it { expect(validator.valid? "3").to eq true }
+    it { expect(validator.valid? "5.6").to eq true }
+    it { expect(validator.valid? -3).to eq true }
+    it { expect(validator.valid? -4.6).to eq true }
+    it { expect(validator.valid? "-3").to eq true }
+    it { expect(validator.valid? "-5.6").to eq true }
 
-  it { expect(validator.valid? "-5.6a").to eq false }
-  it { expect(validator.valid? "a4").to eq false }
+    it { expect(validator.valid? "-5.6a").to eq false }
+    it { expect(validator.valid? "a4").to eq false }
+  end
+       # format, greater_than, less_than, greater_than_or_equal_to
+       # less_than_or_equal_to, equal_to
+  context "with" do
+    let(:validator) { ShibuyaRecord::Validations::Numericality.new(option) }
+
+    context "greater than" do
+      let(:option) { { :greater_than => 3 } }
+
+      it { expect(validator.valid? nil).to eq false }
+      it { expect(validator.valid? []).to eq false }
+      it { expect(validator.valid?({})).to eq false }
+      it { expect(validator.valid? "").to eq false }
+
+      it { expect(validator.valid? 0).to eq false }
+      it { expect(validator.valid? 3).to eq false }
+      it { expect(validator.valid? 6).to eq true }
+      it { expect(validator.valid? 4.6).to eq true }
+      it { expect(validator.valid? 1.6).to eq false }
+      it { expect(validator.valid? "3").to eq false }
+      it { expect(validator.valid? "6").to eq true }
+      it { expect(validator.valid? "5.6").to eq true }
+      it { expect(validator.valid? -3).to eq false }
+      it { expect(validator.valid? -4.6).to eq false }
+      it { expect(validator.valid? "-3").to eq false }
+      it { expect(validator.valid? "-5.6").to eq false }
+
+      it { expect(validator.valid? "-5.6a").to eq false }
+      it { expect(validator.valid? "a4").to eq false }
+    end
+  end
 end
