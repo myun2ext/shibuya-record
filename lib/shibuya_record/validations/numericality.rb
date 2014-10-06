54090320d067d9ce775b9fa9a21b4f9f27118fb7
myun2 <myun2@nwhite.info>
2014-10-06 19:11:43 +0900

    validation numericality update

--- lib/shibuya_record/validations/numericality.rb
+++ lib/shibuya_record/validations/numericality.rb
@@ -1,14 +1,49 @@
 module ShibuyaRecord
   module Validations
     class Numericality
-      def self.valid?(value, options)
-        return true if value.is_a? Numeric
+      #
+      # format, greater_than, less_than, greater_than_or_equal_to
+      # less_than_or_equal_to, equal_to
+      def initialize(options)
+        @options = options
+      end
+
+      def valid?(value)
+        return numeric_validation(value) if value.is_a? Numeric
 
         if value.is_a? String
-          return true if value ~= /\A\d+\z/
+          return numeric_validation(value.to_i) if value =~ format
+          return numeric_validation(value.to_f) if value =~ floating_format
         end
 
         false
+      end
+
+      private
+      def format
+        @options[:format] || /\A-?\d+\z/
+      end
+      def floating_format
+        @options[:format] || /\A-?\d+(\.\d+)?\z/
+      end
+
+      def numeric_validation(value)
+        if @options[:greater_than]
+          return false unless value > @options[:greater_than]
+        end
+        if @options[:less_than]
+          return false unless value < @options[:less_than]
+        end
+        if @options[:greater_than_or_equal_to]
+          return false unless value >= @options[:greater_than_or_equal_to]
+        end
+        if @options[:less_than_or_equal_to]
+          return false unless value <= @options[:less_than_or_equal_to]
+        end
+        if @options[:equal_to]
+          return false unless value == @options[:equal_to]
+        end
+        true
       end
     end
   end
