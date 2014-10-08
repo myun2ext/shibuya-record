7e3ebbca7798a4e2e36823014783ebabb7e07bd4
myun2 <myun2@nwhite.info>
2014-10-08 16:10:15 +0900

    update ALTER TABLE

--- lib/shibuya_record/query_generators/ddl/alter_table.rb
+++ lib/shibuya_record/query_generators/ddl/alter_table.rb
@@ -13,15 +13,30 @@ module ShibuyaRecord
         end
 
         def self.generate(params)
-          table = params[:table_name] || params[:table]
-          action = params[:action].upcase
-          target = params[:target].upcase
+          table =  params[:table_name] || params[:table]
+          action = params[:action].to_s.upcase
+          target = params[:target].to_s.upcase
           definition = params[:definition]
+
+          if target == "COLUMN"
+            column = params[:column] || params[:column_name]
+            type = params[:column_type] || params[:type]
+            definition = "#{column} #{type}"
+          end
+
           query = "ALTER TABLE #{table} #{action} #{target} #{definition}"
         end
 
         def self.add_column(params)
           generate(params.merge({action: "ADD", target: "COLUMN"}))
+        end
+
+        def self.remove_column(params)
+          generate(params.merge({action: "REMOVE", target: "COLUMN"}))
+        end
+
+        def self.change_column(params)
+          generate(params.merge({action: "CHANGE", target: "COLUMN"}))
         end
       end
     end
