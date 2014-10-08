d7974394860569083da74596e585ae821763d04b
myun2 <myun2@nwhite.info>
2014-10-08 18:35:22 +0900

    DDL Create Index

---
+++ lib/shibuya_record/query_generators/ddl/create_index.rb
@@ -0,0 +1,31 @@
+module ShibuyaRecord
+  module QueryGenerators
+    module Ddl
+      class CreateIndex
+        attr_reader :query
+
+        def initialize(params)
+          @params
+        end
+
+        def generate
+          @query = CreateIndex.generate(@params)
+        end
+
+        def self.generate(params)
+          query = "CREATE "
+          query += "UNIQUE " if params[:unique] && params[:unique] != false && params[:unique] != nil
+          table_name = params[:table_name] || params[:table]
+          columns = params[:columns].join(", ")
+          query += "INDEX #{index_name(params)} ON #{table_name} (#{columns})"
+        end
+
+        private
+        def self.index_name(params)
+          table_name = params[:table_name] || params[:table]
+          "index_#{table_name}_on_" + params[:columns].join("_and_")
+        end
+      end
+    end
+  end
+end
