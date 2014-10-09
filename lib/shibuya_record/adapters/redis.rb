4bbe34f79b474191f98c09d5d6464e245ea81de1
myun2 <myun2@nwhite.info>
2014-10-09 13:15:32 +0900

    Redis adapter tested.

--- lib/shibuya_record/adapters/redis.rb
+++ lib/shibuya_record/adapters/redis.rb
@@ -5,19 +5,21 @@ module ShibuyaRecord
     class Redis
       attr_reader :connection
 
-      def initialize(params)
-        @connection = create_connection(params)
+      def initialize(params = {})
+        @connection = ShibuyaRecord::Adapters::Redis.create_connection(params)
       end
 
-      def self.create_connection(params)
-        Redis.new(params)
+      def self.create_connection(params = {})
+        ::Redis.new(params)
       end
 
       def read(key)
         connection.get key; end
+      alias get read
 
       def write(key, value)
         connection.set key, value; end
+      alias set write
 
       def query(query_string, *params)
         connection.send(query_string, params)
