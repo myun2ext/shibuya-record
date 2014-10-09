8d47d9da177233d64f32472795d7ce4a0fd47e73
myun2 <myun2@nwhite.info>
2014-10-09 14:51:21 +0900

    Mongo adapter spec.

--- lib/shibuya_record/adapters/mongo.rb
+++ lib/shibuya_record/adapters/mongo.rb
@@ -6,22 +6,28 @@ module ShibuyaRecord
       attr_reader :connection
 
       def initialize(params)
-        @connection = create_connection(params)
+        param_db_name = params.delete :database
+        @connection = ShibuyaRecord::Adapters::Mongo.create_connection(params)
+
+        select_database(param_db_name) if param_db_name
       end
 
       def self.create_connection(params)
-        Mongo::Connection.new(params)
+        ::Mongo::Connection.new(params)
       end
 
       def database=(name)
-        @db = connection.db(name)
+        @db = connection.db(name.to_s)
       end
-      def database
+      def database(name = nil)
+        self.database = name if name
         @db
       end
+      alias select_database database=
+      alias set_database database=
 
       def collection(name)
-        db.collection(name)
+        @db.collection(name)
       end
       alias table collection
       alias coll collection
@@ -35,7 +41,7 @@ module ShibuyaRecord
       end
 
       def where(table, conditions)
-        coll(table).find(values)
+        coll(table).find(conditions)
       end
     end
   end
