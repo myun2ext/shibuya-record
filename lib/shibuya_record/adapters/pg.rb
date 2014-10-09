db3a5073af0a72e85ae77b6d64c4183792b030df
myun2 <myun2@nwhite.info>
2014-10-09 12:31:44 +0900

    postgres DB driver

---
+++ lib/shibuya_record/adapters/pg.rb
@@ -0,0 +1,21 @@
+require 'pg'
+
+module ShibuyaRecord
+  module Adapters
+    class Postgresql
+      attr_reader :connection
+
+      def initialize(params)
+        @connection = create_connection(params)
+      end
+
+      def self.create_connection(params)
+        PG.connect(params)
+      end
+
+      def query(query_string, *params)
+        connection.exec(query_string, params)
+      end
+    end
+  end
+end
